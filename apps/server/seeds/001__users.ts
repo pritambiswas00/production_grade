8659e6ace5daebf0fbf28fa3478f0239df062f19
Pritam Biswas <pritambiswas060@gmail.com>
2024-04-30 13:29:22 +0530

    feat: add DB, docs, and routes for the API

---
+++ apps/server/seeds/001__users.ts
@@ -0,0 +1,51 @@
+import { Knex } from 'knex';
+import { IUser } from '../src/validationSchema/types';
+
+export async function seed(knex: Knex): Promise<void> {
+  // Deletes ALL existing entries
+  await knex('users').del();
+
+  // Inserts seed entries
+  await knex<IUser>('users').insert([
+    {
+      id: 1,
+      created_at: new Date().toString(),
+      email: 'pritambiswas1@gmail.com',
+      name: 'Pritam Biswas',
+      updated_at: new Date().toString(),
+      password: 'sdsdsdsd',
+    },
+    {
+      id: 2,
+      created_at: new Date().toString(),
+      email: 'pritambiswas2@gmail.com',
+      name: 'Pritam Biswas',
+      updated_at: new Date().toString(),
+      password: 'sdsdsdsd',
+    },
+    {
+      id: 3,
+      created_at: new Date().toString(),
+      email: 'pritambiswas3@gmail.com',
+      name: 'Pritam Biswas',
+      updated_at: new Date().toString(),
+      password: 'dsdssdsd',
+    },
+    {
+      id: 4,
+      created_at: new Date().toString(),
+      email: 'pritambiswas4@gmail.com',
+      name: 'Pritam Biswas',
+      updated_at: new Date().toString(),
+      password: 'sfsddsds',
+    },
+    {
+      id: 5,
+      created_at: new Date().toString(),
+      email: 'pritambiswas5@gmail.com',
+      name: 'Pritam Biswas',
+      updated_at: new Date().toString(),
+      password: 'ssdsds',
+    },
+  ]);
+}
