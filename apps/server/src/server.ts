d16ee75b72326352754022e2cca13609b0f0f865
Pritam Biswas <pritambiswas060@gmail.com>
2024-05-14 16:35:02 +0530

    feat: updated

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -52,7 +52,7 @@ appInstance.use(
         user: serverConfig.DB_USER,
         password: serverConfig.PASSWORD,
         port: Number(serverConfig.DB_PORT),
-        host: process.env.DB_SERVER,
+        host: 'aws-0-ap-south-1.pooler.supabase.com',
       },
       ttl: 1000 * 60 * 60 * 24,
     }),
